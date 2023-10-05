13acb02d5995c9a2f9da65ab1cb13e5c33ef3cc3
João Paulo <jpresendejava@gmail.com>
2023-10-05 09:55:00 -0400

    Creation of the end point Customer registration

--- Controller/ClienteCommandController.cs
+++ Controller/ClienteCommandController.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using TableandCommandControl.Context;
+using TableandCommandControl.DTO;
 using TableandCommandControl.Entity;
 
 namespace TableandCommandControl.Controller
 {
-  [Route("[controller]")]
-    public class ClienteCommandController: ControllerBase
+    [Route("[controller]")]
+    public class ClienteCommandController : ControllerBase
     {
         private readonly TableComandContext _context;
 
@@ -20,32 +22,53 @@ namespace TableandCommandControl.Controller
         {
             _context = context;
         }
-[HttpGet]
-public IActionResult ListClient()
-{
-    var clients = _context.Clients
-        .Include(c => c.dependents)
-        .ToList();
-
-    var clientData = clients.Select(client => new
-    {
-        id = client.id,
-        name = client.name,
-        lastName = client.lastName,
-        cpf = client.CPF,
-        rg = client.RG,
-        dependents = client.dependents.Select(dependent => new
+        [HttpGet]
+        public IActionResult ListClient()
         {
-            id = dependent.id,
-            dependentType = ((DependentType)dependent.dependentType).ToString(),
-            clientId = dependent.clientId
-        }).ToList()
-    }).ToList();
+            var clients = _context.Clients
+                .Include(c => c.dependents)
+                .ToList();
 
-    return Ok(clientData);
-}
+            var clientData = clients.Select(client => new
+            {
+                id = client.id,
+                name = client.name,
+                lastName = client.lastName,
+                cpf = client.CPF,
+                rg = client.RG,
+                dependents = client.dependents.Select(dependent => new
+                {
+                    id = dependent.id,
+                    dependentType = ((DependentType)dependent.dependentType).ToString(),
+                    clientId = dependent.clientId
+                }).ToList()
+            }).ToList();
+
+            return Ok(clientData);
+        }
+
+        [HttpPost]
+        public IActionResult CreateClient([FromBody]ClientDto clientDto){
+
+            var cad = new Client{
+                name = clientDto.name,
+                lastName = clientDto.lastName,
+                CPF = clientDto.CPF,
+                RG = clientDto.RG
+
+            };
 
 
+            _context.Clients.Add(cad);
+            _context.SaveChanges();
+
+            var res = new {
+               Menssage = "customer registered successfully!",
+               ClietCad =  cad,
+            };
+
+            return Ok(res);
+        }
 
     }
 }
---
+++ DTO/ClientDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TableandCommandControl.DTO
+{
+    public class ClientDto
+    {
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+    }
+}
